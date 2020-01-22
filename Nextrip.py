d81ca90854ea7ae7e4e3b6597a8dd4592c48ff9f
Jacob Zukauska <j.zukauska@gmail.com>
2020-01-22 00:24:52 -0600

    Improve testing.Add scaffolding for Nextrip.

--- Nextrip.py
+++ Nextrip.py
@@ -1,24 +1,40 @@
 from Direction import Direction as D
 
 
-
 class Nextrip():
     def __init__(self, user_route, user_stop, user_direction):
         self.__route = user_route
         self.__stop = user_stop
         self.__direction = self.__direction_convert(user_direction)
 
-    def __direction_convert(self,value):
+    @staticmethod
+    def __direction_convert(value):
         lower = value.lower()
         try:
-            direction_number = D[value].value
-        except KeyError :
+            direction_number = D[lower].value
+        except KeyError:
             print("Cardinal direction {} is not a valid direction".format(value))
-
+            raise
         else:
-            return(direction_number)
+            return direction_number
 
+    @property
+    def route(self):
+        return self.__route
 
+    def __get_routes(self):
+        pass
 
+    def __get_stops(self):
+        pass
 
+    def __getdeparture(self):
 
+        pass
+
+    def __str__(self):
+        try:
+            self.__getdeparture()
+
+        finally:
+            return ("Good")
--- test/test_NextripClass.py
+++ test/test_NextripClass.py
@@ -7,12 +7,15 @@ class TestNextTrip(unittest.TestCase):
 
     def test_user_input(self):
         # Assert error for incorrect direction input
-        self.assertRaises(KeyError, N._Nextrip__direction_convert(N, "weast"))
 
-        bus_north = N("test", "test", "north")
-        bus_south = N("test", "test", "south")
-        bus_east = N("test", "test", "east")
-        bus_west = N("test", "test", "west")
+
+        with self.assertRaises(KeyError):
+            N("test","test","weast")
+
+        bus_north = N("test", "test", "noRth")
+        bus_south = N("test", "test", "sOUth")
+        bus_east = N("test", "test", "East")
+        bus_west = N("test", "test", "WEST")
         self.assertEqual(bus_north._Nextrip__direction, D['north'].value)
         self.assertEqual(bus_south._Nextrip__direction, D["south"].value)
         self.assertEqual(bus_east._Nextrip__direction, D["east"].value)
@@ -20,4 +23,4 @@ class TestNextTrip(unittest.TestCase):
 
         pass
 
-    def
+
