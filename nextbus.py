663591f115932fef68b466b451655999ca7858dd
Jacob Zukauska <j.zukauska@gmail.com>
2020-01-22 00:25:19 -0600

    Add argument parsing

--- nextbus.py
+++ nextbus.py
@@ -1,8 +1,30 @@
 import requests
 import json
 import pickle
-import Nextrip
+from Nextrip import Nextrip
+import argparse
+import sys
+
+debugMode = False
+
+if not debugMode:
+    sys.tracebacklimit = 0
+
+
 
 if __name__ == '__main__':
-    response = (requests.get('https://api.github.com'))
-    print(response.status_code)
+    parser = argparse.ArgumentParser(description='Get next bus time')
+    parser.add_argument('route', type=str, help='Substring of the bus route name')
+    parser.add_argument('stop', type=str, help='Substring of the bus stop name')
+    parser.add_argument('direction', type=str, help='"North” “East” “West” or “South”')
+
+    args = parser.parse_args()
+
+    route = args.route
+    stop = args.stop
+    direction = args.direction
+
+    bus = Nextrip(route,stop,direction)
+
+
+
