59b18e3efbc25ec14ba3abce3115a8c7f0a26d82
Jacob Zukauska <j.zukauska@gmail.com>
2020-01-21 23:11:55 -0600

    Fixed test to suit correct handling

--- Nextrip.py
+++ Nextrip.py
@@ -1,18 +1,24 @@
 from Direction import Direction as D
 
 
+
 class Nextrip():
     def __init__(self, user_route, user_stop, user_direction):
         self.__route = user_route
         self.__stop = user_stop
-        self.__direction = self.__direction(user_direction)
+        self.__direction = self.__direction_convert(user_direction)
 
-    def __direction(self,value):
+    def __direction_convert(self,value):
         lower = value.lower()
         try:
-            direction_number = repr( D[value]).value
-        except KeyError:
-            raise KeyError
+            direction_number = D[value].value
+        except KeyError :
             print("Cardinal direction {} is not a valid direction".format(value))
+
         else:
             return(direction_number)
+
+
+
+
+
--- test/test_NextripClass.py
+++ test/test_NextripClass.py
@@ -1,10 +1,23 @@
 import unittest
 from Nextrip import Nextrip as N
+from Direction import Direction as D
+
 
 class TestNextTrip(unittest.TestCase):
 
     def test_user_input(self):
         # Assert error for incorrect direction input
-        self.assertRaises(KeyError,N,"test","test","weast")
+        self.assertRaises(KeyError, N._Nextrip__direction_convert(N, "weast"))
+
+        bus_north = N("test", "test", "north")
+        bus_south = N("test", "test", "south")
+        bus_east = N("test", "test", "east")
+        bus_west = N("test", "test", "west")
+        self.assertEqual(bus_north._Nextrip__direction, D['north'].value)
+        self.assertEqual(bus_south._Nextrip__direction, D["south"].value)
+        self.assertEqual(bus_east._Nextrip__direction, D["east"].value)
+        self.assertEqual(bus_west._Nextrip__direction, D["west"].value)
 
         pass
+
+    def
